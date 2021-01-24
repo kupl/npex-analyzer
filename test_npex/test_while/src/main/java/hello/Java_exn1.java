884f55297de1edf46c3cf159f539736c376face7
intotking <junhee_lee@korea.ac.kr>
2021-01-24 18:32:38 +0900

    update testcase

--- test_npex/test_while/src/main/java/hello/Java_exn1.java
+++ test_npex/test_while/src/main/java/hello/Java_exn1.java
@@ -1,25 +1,33 @@
 package hello;
+import java.io.InputStream;
 
 class A {
   A f;
-
-  public A get_f() {
-    return this.f;
-  }
+  int i;
+  
+  public int get_i() { return i; }
+  public A get_f() { return this.f; }
 }
 
 public class Java_exn1 {
 static boolean main(hello.A arg) {
     int n;
     int ret = 0;
-    hello.A np = arg;/* np: non null pointer */
+    try {
+      byte[] buffer = new byte[256];
+      hello.A np = arg;/* np: non null pointer */
 
-    while ((n = np.i) != (-1)) {
-        np = arg.get_f();/* np: null pointer */
+      while ((n = System.in.read(buffer)) != (-1)) {
+          np = arg.get_f();/* np: null pointer */
 
-        ret = ret + np.i;
-    } 
-    return ret == 0;
+          ret = ret + np.i;
+      } 
+      return ret == 0;
+    }
+    catch (Exception e) 
+    {
+      return false;
+    }
 }
   /* specification:
    * np == null -> n == -1 */
