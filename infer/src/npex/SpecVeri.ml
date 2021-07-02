9e82126163916e39e9c5686b112733eac4dcbc9c
intotking <junhee_lee@korea.ac.kr>
2021-07-02 18:54:09 +0900

    [SpecVeri] 1. fix scalability to find spec with maximum probabilty, 2. pretty print results, 3 check sat and validity of specs with uncaughted NPE information

--- infer/src/npex/SpecVeri.ml
+++ infer/src/npex/SpecVeri.ml
@@ -11,6 +11,22 @@ module Models = PrettyPrintable.MakePPMonoMap (ModelKey) (MValues)
 module Val = Domain.Val
 module APSet = AbstractDomain.FiniteSet (AccessExpr)
 
+let score spec =
+  let applied_models =
+    List.fold ~init:NullModel.empty spec ~f:(fun acc Domain.{applied_models} ->
+        NullModel.union (fun _ lhs _ -> Some lhs) acc applied_models)
+  in
+  NullModel.compute_probability applied_models
+
+
+let print_spec spec =
+  let applied_models =
+    List.fold ~init:NullModel.empty spec ~f:(fun acc Domain.{applied_models} ->
+        NullModel.union (fun _ lhs _ -> Some lhs) acc applied_models)
+  in
+  L.progress " - %f (%a)@." (score spec) NullModel.pp applied_models
+
+
 module Spec = struct
   type t = {pc: Formula.t; output: Formula.t; symbols: Val.Set.t; uncaught_npes: APSet.t} [@@deriving compare]
 
@@ -24,8 +40,9 @@ module Spec = struct
     [@@ocamlformat "disable"]
 
   let check_sat ?(print_unsat = false) (infered : t) (patched : t) =
-    let result = Formula.check_sat ~print_unsat infered.pc patched.pc in
-    result
+    let pc_sat = Formula.check_sat ~print_unsat infered.pc patched.pc in
+    let uncaught_sat = Bool.equal (APSet.is_empty infered.uncaught_npes) (APSet.is_empty patched.uncaught_npes) in
+    pc_sat && uncaught_sat
 
 
   let check_valid ?(print_invalid = false) (infered : t) (patched : t) =
@@ -45,7 +62,7 @@ module Spec = struct
     {pc; output; symbols; uncaught_npes}
 
 
-  let joinable lhs rhs = Formula.check_sat lhs.pc rhs.pc
+  let joinable lhs rhs = check_sat lhs rhs
 
   let from_state proc_desc astate =
     let pc, output = StateFormula.from_state proc_desc astate in
@@ -54,7 +71,11 @@ module Spec = struct
       let null_values = Domain.(astate.uncaught_npes) in
       Val.Set.fold (APSet.union <<< StateFormula.val_to_ap astate) (Val.Set.of_list null_values) APSet.empty
     in
-    {pc; output; symbols; uncaught_npes}
+    let result = {pc; output; symbols; uncaught_npes} in
+    L.progress "@.========= State to Spec ===========@." ;
+    L.progress "%a@.------------------------------Specification-------------------@." Domain.pp astate ;
+    L.progress "%a@." pp result ;
+    result
 end
 
 module Specs = struct
@@ -64,74 +85,26 @@ module Specs = struct
     join_list specs_to_clarify ~joinable:Spec.joinable ~join:Spec.join ~pp:Spec.pp
 
 
-  let from_disjuncts proc_desc disjuncts = disjuncts |> List.map ~f:(Spec.from_state proc_desc) |> clarify_specs
+  let from_disjuncts proc_desc disjuncts =
+    L.progress "[PROGRESS]: convert disjuncts to specs@." ;
+    disjuncts |> List.map ~f:(Spec.from_state proc_desc) |> clarify_specs
 end
 
-let score spec =
-  let applied_models =
-    List.fold ~init:NullModel.empty spec ~f:(fun acc Domain.{applied_models} ->
-        NullModel.union (fun _ lhs _ -> Some lhs) acc applied_models)
-  in
-  NullModel.compute_probability applied_models
+module InferedStates = struct
+  include PrettyPrintable.MakePPMap (NullPoint)
+
+  let update k v = update k (function Some v' -> Some (v :: v') | None -> Some [v])
+
+  let from_disjuncts disjuncts =
+    List.fold disjuncts ~init:empty ~f:(fun acc astate ->
+        match Domain.(astate.fault) with
+        | Some nullpoint ->
+            update nullpoint astate acc
+        | None ->
+            update NullPoint.dummy astate acc)
 
 
-let print_spec spec =
-  let applied_models =
-    List.fold ~init:NullModel.empty spec ~f:(fun acc Domain.{applied_models} ->
-        NullModel.union (fun _ lhs _ -> Some lhs) acc applied_models)
-  in
-  L.progress " - %f (%a)@." (score spec) NullModel.pp applied_models
-
-
-let rec add_mergeable_model acc worklist : Domain.t list list =
-  L.progress " - number of worklist : %d@." (List.length worklist) ;
-  let joinable lhs rhs = NullModel.joinable Domain.(lhs.applied_models) Domain.(rhs.applied_models) in
-  let insertable states to_add = List.for_all states ~f:(joinable to_add) in
-  let is_final (selected, remain, abandoned) =
-    List.is_empty remain && not (List.exists abandoned ~f:(insertable selected))
-  in
-  match worklist with
-  | [] ->
-      acc
-  | work :: rest -> (
-      let selected, remain, abandoned = work in
-      match remain with
-      | [] when is_final work ->
-          add_mergeable_model (selected :: acc) rest
-      | [] ->
-          add_mergeable_model acc rest
-      | hd :: tl when insertable selected hd ->
-          let work_hd_added = (hd :: selected, tl, abandoned) in
-          let work_hd_dropped = (selected, tl, hd :: abandoned) in
-          add_mergeable_model acc (work_hd_added :: work_hd_dropped :: rest)
-      | hd :: tl ->
-          let work_hd_dropped = (selected, tl, hd :: abandoned) in
-          add_mergeable_model acc (work_hd_dropped :: rest) )
-
-
-let select_most_probable_spec model_cands =
-  let has_uncaught_exceptions model_cand = List.exists model_cand ~f:Domain.has_uncaught_model_npes in
-  let model_cands =
-    List.filter model_cands ~f:(not <<< has_uncaught_exceptions) |> List.filter ~f:(not <<< List.is_empty)
-  in
-  let max_opt =
-    List.max_elt model_cands ~compare:(fun lhs rhs ->
-        Int.of_float (score lhs *. 100.) - Int.of_float (score rhs *. 100.))
-  in
-  L.progress "==== Model with score ====@." ;
-  (* List.iter model_cands ~f:print_spec ; *)
-  match max_opt with
-  | Some max ->
-      L.progress "@.==== MAX Model ====@." ; print_spec max ; max
-  | None ->
-      L.(die InternalError) "No spec"
-
-
-let verify proc_desc summary_inferenced summary_patched =
-  let specs_inferenced, specs_normal = List.partition_tf summary_inferenced ~f:Domain.is_npe_alternative in
-  let specs_normal = Specs.from_disjuncts proc_desc specs_normal in
-  L.progress " - size of specs_inferenced : %d@." (List.length specs_inferenced) ;
-  let all_candidate_model =
+  let all_candidate_model specs_inferenced =
     let merged_model =
       List.fold specs_inferenced ~init:Models.empty ~f:(fun acc Domain.{applied_models} ->
           NullModel.fold
@@ -149,14 +122,18 @@ let verify proc_desc summary_inferenced summary_patched =
     in
     L.progress " - merged model : %a@." Models.pp merged_model ;
     L.progress "================================================@." ;
-    Models.fold
-      (fun k mvalues acc ->
-        List.concat_map (MValues.elements mvalues) ~f:(fun mvalue ->
-            List.map acc ~f:(fun model -> Model.add k mvalue model)))
-      merged_model [Model.empty]
-  in
-  L.progress " - all possible model values@." ;
-  L.progress "%a@." (Pp.seq ~sep:"\n=====================================\n" Model.pp) all_candidate_model ;
+    let result =
+      Models.fold
+        (fun k mvalues acc ->
+          List.concat_map (MValues.elements mvalues) ~f:(fun mvalue ->
+              List.map acc ~f:(fun model -> Model.add k mvalue model)))
+        merged_model [Model.empty]
+    in
+    L.progress " - all possible model values@." ;
+    L.progress "%a@." (Pp.seq ~sep:"\n----------------------------------------------" Model.pp) result ;
+    result
+
+
   let collect_feasible_state states model =
     let is_feasible model Domain.{applied_models} =
       NullModel.for_all
@@ -175,44 +152,143 @@ let verify proc_desc summary_inferenced summary_patched =
         applied_models
     in
     List.filter states ~f:(is_feasible model)
+
+
+  let select_most_probable_spec model_cands =
+    let has_uncaught_exceptions model_cand = List.exists model_cand ~f:Domain.has_uncaught_model_npes in
+    let model_cands =
+      List.filter model_cands ~f:(not <<< has_uncaught_exceptions) |> List.filter ~f:(not <<< List.is_empty)
+    in
+    let max_opt =
+      List.max_elt model_cands ~compare:(fun lhs rhs ->
+          Int.of_float (score lhs *. 100.) - Int.of_float (score rhs *. 100.))
+    in
+    List.iter model_cands ~f:(fun model_cand ->
+        let model = Domain.((List.hd_exn model_cand).applied_models) in
+        L.progress "Model with score %d:@.   %a@." (Int.of_float (score model_cand *. 100.)) NullModel.pp model) ;
+    L.progress "==== Model with score ====@." ;
+    (* List.iter model_cands ~f:print_spec ; *)
+    match max_opt with
+    | Some max ->
+        L.progress "@.==== MAX Model ====@." ; print_spec max ; max
+    | None ->
+        L.(die InternalError) "No spec"
+
+
+  let normal_and_max_from_disjuncts disjuncts =
+    let inferenced, normal = List.partition_tf disjuncts ~f:Domain.is_npe_alternative in
+    let all_candidate_model = all_candidate_model inferenced in
+    let feasible_states_list = List.map all_candidate_model ~f:(collect_feasible_state inferenced) in
+    L.progress " - number of possible states: %d@." (List.length feasible_states_list) ;
+    let specs_inferenced = feasible_states_list |> select_most_probable_spec in
+    (specs_inferenced, normal)
+
+
+  let pp fmt x =
+    F.fprintf fmt "@[<v 2>Inferenced States@," ;
+    iter
+      (fun nullpoint disjuncts ->
+        F.fprintf fmt "@[<v 2> States of %a: {@," NullPoint.pp nullpoint ;
+        List.iter disjuncts ~f:(fun astate -> F.fprintf fmt "%a@," Domain.pp astate) ;
+        F.fprintf fmt "}@]")
+      x ;
+    F.fprintf fmt "@]"
+
+
+  let pp_max fmt x =
+    let max_spec, normal = normal_and_max_from_disjuncts x in
+    F.fprintf fmt "@[<v 2>Normal States@," ;
+    List.iter normal ~f:(fun astate -> F.fprintf fmt "  @[%a@]@," Domain.pp astate) ;
+    F.fprintf fmt "Inferenced States@," ;
+    List.iter max_spec ~f:(fun astate -> F.fprintf fmt "  @[%a@]@," Domain.pp astate) ;
+    F.fprintf fmt "@]"
+end
+
+let pp_states fmt x = InferedStates.pp fmt (InferedStates.from_disjuncts x)
+
+let pp_max = InferedStates.pp_max
+
+let _to_print = ref String.Map.empty
+
+let add_print ~category ~msg =
+  _to_print :=
+    String.Map.change !_to_print category ~f:(function
+      | Some msgs ->
+          Some (F.asprintf "%s@,%s" msgs msg)
+      | None ->
+          Some (F.asprintf "%s" msg))
+
+
+let print_result () =
+  let patch_id = match Config.npex_patch_id with Some patch_id -> patch_id | None -> "unknown_patch" in
+  String.Map.iteri !_to_print ~f:(fun ~key ~data ->
+      let msg = F.asprintf "@[<v 2> %s:@, %s@]" key data in
+      print_to_file ~msg ~filename:(Config.npex_result_dir ^/ patch_id ^ "_" ^ key))
+
+
+let add_print_state ~category astate = add_print ~category ~msg:(F.asprintf "%a" Domain.pp astate)
+
+let add_print_spec ~category spec = add_print ~category ~msg:(F.asprintf "%a" Spec.pp spec)
+
+let verify proc_desc summary_inferenced summary_patched =
+  let states_inferenced, states_normal = InferedStates.normal_and_max_from_disjuncts summary_inferenced in
+  (* L.progress " - size of specs_inferenced : %d@." (List.length specs_inferenced) ;
+     if Config.debug_mode then
+       L.progress " ============ Inferenced States ============== @.%a@.@.@.@."
+         (Pp.seq ~sep:"\n---------------------------------------" Domain.pp)
+         specs_inferenced ; *)
+  let specs_normal, specs_inferenced =
+    (Specs.from_disjuncts proc_desc states_normal, Specs.from_disjuncts proc_desc states_inferenced)
   in
-  let feasible_states_list = List.map all_candidate_model ~f:(collect_feasible_state specs_inferenced) in
-  L.progress " - number of possible states: %d@." (List.length feasible_states_list) ;
-  let specs_inferenced = feasible_states_list |> select_most_probable_spec |> Specs.from_disjuncts proc_desc in
-  let specs_patched, specs_patched_normal = List.partition_tf summary_patched ~f:Domain.is_npe_alternative in
+  let states_patched, states_patched_normal = List.partition_tf summary_patched ~f:Domain.is_npe_alternative in
   let specs_patched, specs_patched_normal =
-    (Specs.from_disjuncts proc_desc specs_patched, Specs.from_disjuncts proc_desc specs_patched_normal)
+    (Specs.from_disjuncts proc_desc states_patched, Specs.from_disjuncts proc_desc states_patched_normal)
   in
-  let print_spec i spec = L.progress "==== %d-th spec ====@.%a@." i Spec.pp spec in
-  L.progress " - # of inferenced states: %d@." (List.length specs_inferenced) ;
-  if Config.debug_mode then List.iteri specs_inferenced ~f:print_spec ;
-  L.progress "@.================================================================@." ;
-  L.progress " - # of patched states: %d@." (List.length specs_patched) ;
-  if Config.debug_mode then List.iteri specs_patched ~f:print_spec ;
+  List.iter states_inferenced ~f:(add_print_state ~category:"inferenced_states") ;
+  List.iter states_normal ~f:(add_print_state ~category:"normal_states") ;
+  List.iter states_patched ~f:(add_print_state ~category:"patched_states") ;
+  List.iter states_patched_normal ~f:(add_print_state ~category:"patched_normal_states") ;
+  List.iter specs_normal ~f:(add_print_spec ~category:"normal_specs") ;
+  List.iter specs_inferenced ~f:(add_print_spec ~category:"inferenced_specs") ;
+  List.iter specs_patched ~f:(add_print_spec ~category:"_patched_specs") ;
+  List.iter specs_patched_normal ~f:(add_print_spec ~category:"_patched_normal_specs") ;
+  (* let print_spec i spec = L.progress "==== %d-th spec ====@.%a@." i Spec.pp spec in
+     L.progress " - # of inferenced states: %d@." (List.length specs_inferenced) ;
+     if Config.debug_mode then List.iteri specs_inferenced ~f:print_spec ;
+     L.progress "@.================================================================@." ;
+     L.progress " - # of patched states: %d@." (List.length specs_patched) ;
+     if Config.debug_mode then List.iteri specs_patched ~f:print_spec ; *)
   let verify ~specs_inferenced ~specs_patched =
     let rec compute_tuple specs_inferenced_rest specs_patched_matched acc =
       match specs_inferenced_rest with
-      | [] when Specs.equal specs_patched_matched (Specs.of_list specs_patched) ->
-          acc
-      | [] ->
+      | []
+        when Specs.is_empty specs_patched_matched
+             || not (Specs.equal specs_patched_matched (Specs.of_list specs_patched)) ->
           let unmatched_specs = Specs.diff (Specs.of_list specs_patched) specs_patched_matched in
-          L.progress "no satisfied spec inferenced to the following patched spec@." ;
-          L.progress "%a@." Specs.pp unmatched_specs ;
-          L.progress "--------------------inferenced specs----------------------" ;
-          L.progress "%a@." Specs.pp (Specs.of_list specs_inferenced) ;
+          add_print ~category:"FAIL" ~msg:"unmatched patched specs exist:" ;
+          add_print ~category:"FAIL" ~msg:(F.asprintf "%a" Specs.pp unmatched_specs) ;
+          add_print ~category:"FAIL" ~msg:"--------------------inferenced specs----------------------" ;
+          add_print ~category:"FAIL" ~msg:(F.asprintf "%a" Specs.pp (Specs.of_list specs_inferenced)) ;
           (* unmatched spec-patch exists *) []
+      | [] ->
+          acc
       | spec_inferenced :: specs_inferenced_rest' ->
           let sat_specs = List.filter specs_patched ~f:(Spec.check_sat spec_inferenced) in
           if List.is_empty sat_specs then (
             ignore (List.filter specs_patched ~f:(Spec.check_sat ~print_unsat:true spec_inferenced)) ;
-            L.progress "no satisfiable spec to the following inferenced spec@." ;
-            L.progress "%a@." Spec.pp spec_inferenced ;
-            L.progress "--------------------patched specs----------------------" ;
-            L.progress "%a@." (Pp.seq ~sep:"\n" Spec.pp) specs_patched ;
+            (* add_print ~category:"FAIL" ~msg:"no satisfied patched spec to the following inferenced specs" ;
+               add_print ~category:"FAIL" ~msg:(F.asprintf "%a" Specs.pp (Specs.of_list specs_inferenced)) ;
+               add_print ~category:"FAIL" ~msg:"--------------------patched specs----------------------" ;
+               add_print ~category:"FAIL" ~msg:(F.asprintf "%a" Specs.pp (Specs.of_list specs_patched)) ; *)
             if Formula.is_valid (StateFormula.exception_cond proc_desc true) Spec.(spec_inferenced.pc) then
               (* IGNORE IT: inferenced state may have more errors than patched *)
               compute_tuple specs_inferenced_rest' specs_patched_matched acc
-            else [] )
+            else (
+              add_print ~category:"FAIL" ~msg:"no satisfied patched spec to the following inferenced spec" ;
+              add_print ~category:"FAIL" ~msg:(F.asprintf "%a" Spec.pp spec_inferenced) ;
+              add_print ~category:"FAIL" ~msg:"--------------------patched specs----------------------" ;
+              add_print ~category:"FAIL" ~msg:(F.asprintf "%a" Specs.pp (Specs.of_list specs_patched)) ;
+              [] ) )
           else
             let specs_patched_matched' = Specs.union specs_patched_matched (Specs.of_list sat_specs) in
             compute_tuple specs_inferenced_rest' specs_patched_matched' ((spec_inferenced, sat_specs) :: acc)
@@ -220,8 +296,8 @@ let verify proc_desc summary_inferenced summary_patched =
     let tuples = compute_tuple specs_inferenced Specs.empty [] in
     if Config.debug_mode then
       List.iter tuples ~f:(fun (infered, patched) ->
-          L.progress "-----------------------------------@.(%a, %a)@." Spec.pp infered Specs.pp
-            (Specs.of_list patched)) ;
+          add_print ~category:"paired_spec"
+            ~msg:(F.asprintf "(%a, %a)" Spec.pp infered Specs.pp (Specs.of_list patched))) ;
     if List.is_empty tuples then false
     else
       List.for_all tuples ~f:(fun (spec_inferenced, satisfiable_specs) ->
@@ -230,10 +306,10 @@ let verify proc_desc summary_inferenced summary_patched =
             let invalid_specs =
               List.filter satisfiable_specs ~f:(not <<< Spec.check_valid ~print_invalid:true spec_inferenced)
             in
-            L.progress "invalid specs inferenced with respect to the following infered spec@." ;
-            L.progress "%a@." Spec.pp spec_inferenced ;
-            L.progress "-----------------------invalid patched specs--------------------------" ;
-            L.progress "%a@." (Pp.seq ~sep:"\n" Spec.pp) invalid_specs ) ;
+            add_print ~category:"FAIL" ~msg:"invalid specs inferenced with respect to the following infered spec" ;
+            add_print ~category:"FAIL" ~msg:(F.asprintf "%a@." Spec.pp spec_inferenced) ;
+            add_print ~category:"FAIL" ~msg:"-----------------------invalid patched specs-----------------------" ;
+            add_print ~category:"FAIL" ~msg:(F.asprintf "%a@." Specs.pp (Specs.of_list invalid_specs)) ) ;
           is_valid)
   in
   if List.is_empty specs_normal then (
@@ -254,7 +330,9 @@ let launch ~get_summary ~get_original_summary =
   let result = verify target_pdesc summary_inferenced summary_patched in
   if result then (
     L.progress "[SUCCESS]: the patch is verified w.r.t. specification@." ;
+    print_result () ;
     L.exit 0 )
   else (
     L.progress "[FAIL]: the patch does not satisfy specification@." ;
+    print_result () ;
     L.exit 1 )
